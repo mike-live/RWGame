f189274ce3d6aa1ad19de439af6a20b745dafe25
Sergei Tikhomirov <sergey.zr.co.uk@gmail.com>
2020-11-01 16:27:48 +0300

    intermediate

--- RWGame/RWGame/Views/LoginPage.xaml.cs
+++ RWGame/RWGame/Views/LoginPage.xaml.cs
@@ -1,11 +1,5 @@
 ﻿using RWGame.Classes;
 using RWGame.ViewModels;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Xamarin.Essentials;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
