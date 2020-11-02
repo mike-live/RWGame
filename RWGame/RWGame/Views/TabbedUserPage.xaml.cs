07d2754c13db781d3f3385cf0982c196917a3992
Sergei Tikhomirov <sergey.zr.co.uk@gmail.com>
2020-11-02 21:24:41 +0300

    Moved TabbedUserPage

---
+++ RWGame/RWGame/Views/TabbedUserPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using RWGame.Classes;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace RWGame.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class TabbedUserPage : TabbedPage
+    {
+        public TabbedUserPage(ServerWorker serverWorker, SystemSettings systemSettings)
+        {
+            InitializeComponent();
+            NavigationPage.SetHasNavigationBar(this, false);
+            Children.Add(new UserPage(serverWorker, systemSettings, Navigation));
+            Children.Add(new GameHistoryPage(serverWorker, systemSettings, Navigation));           
+        }
+    }
+}
